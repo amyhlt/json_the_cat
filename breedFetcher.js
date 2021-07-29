1bac576682e56ab4f3d8938f2be63e6833de4a8e
AmyTang <huilitang1@gmail.com>
2021-07-29 12:50:32 -0700

    Cat Breedsearch

--- breedFetcher.js
+++ breedFetcher.js
@@ -5,19 +5,18 @@ const breed = args;
 const url =`https://api.thecatapi.com/v1/breeds/search?q=${breed}`;
 
 if (breed === undefined) {
-console.log("Please enter the breed name!");
+  console.log("Please enter the breed name!");
 } else {
-fetch(url)
-.then(response =>
-response.json())
-.then(json => {
-if (json.length === 0) {
-console.log(`${breed} No found `);
-}
-else {
-console.log(json);
-}})
-.catch ((error) => {
-console.log(error);
-});
+  fetch(url)
+  .then(response =>
+    response.json())
+  .then(json => {
+  if (json.length === 0) {
+    console.log(`${breed} No found `);
+  } else {
+    console.log(json);
+  }})
+  .catch ((error) => {
+    console.log(error);
+  });
 }
